e958143abdff26f0c093100b7ed14bbd92d2877c
karthik9724 <kumarkarthik84192@gmail.com>
2025-05-20 19:26:46 +0530

    d11

--- a11/script.js
+++ a11/script.js
@@ -1,23 +1,11 @@
-// for(let i=1;i<=10;i++){
-//     if(i%2!=0){
-//         console.log(i);
-//     }
-// }
-
-
-var fruits=["apple","banana","orange","melon"];
-fruits.map((value)=>{
-    list.innerHTML+=`<li>${value}</li>`
-})
-let str=""
-function disp(){
-    str=document.getElementById('ip').value.toLowerCase();
-    const filtered=fruits.find((value)=>{
-        value.toLoweCase()==str;
-    })
-    if(filtered){
-        di.innerHTML=`Found:${filtered}`
+function disp() {
+    str = document.getElementById('ip').value.toLowerCase();
+    const filtered = fruits.find((value) => {
+        return value.toLowerCase() == str;
+    });
+    if (filtered) {
+        di.innerHTML = `Found: ${filtered}`;
+    } else {
+        di.innerHTML = `Not found`;
     }
-    else
-      di.innerHTML=`Not found`
 }
