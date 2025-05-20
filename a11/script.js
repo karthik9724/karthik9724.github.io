dc5dc9cf7d823345fcbf199c0301e80215cb0356
karthik9724 <kumarkarthik84192@gmail.com>
2025-05-20 19:32:16 +0530

    D11

--- a11/script.js
+++ a11/script.js
@@ -1,11 +1,21 @@
+// Array of fruits
+var fruits = ["apple", "banana", "orange", "melon"];
+
+// Display the fruit list in <ul>
+fruits.map((value) => {
+    document.getElementById('list').innerHTML += `<li>${value}</li>`;
+});
+
+// Function to check if input matches a fruit
 function disp() {
-    str = document.getElementById('ip').value.toLowerCase();
+    let str = document.getElementById('ip').value.toLowerCase();
     const filtered = fruits.find((value) => {
-        return value.toLowerCase() == str;
+        return value.toLowerCase() === str;
     });
+
     if (filtered) {
-        di.innerHTML = `Found: ${filtered}`;
+        document.getElementById('di').innerHTML = `Found: ${filtered}`;
     } else {
-        di.innerHTML = `Not found`;
+        document.getElementById('di').innerHTML = `Not found`;
     }
 }
