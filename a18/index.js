c7f6e2a78bb7ac8fa7d477aa850dd921fc553041
karthik9724 <kumarkarthik84192@gmail.com>
2025-05-20 20:15:04 +0530

    d18

---
+++ a18/index.js
@@ -0,0 +1,7 @@
+// const score=[45,23,78]
+// const [a,...arrscore]=score restoperator
+// console.log(a) 
+
+const item={id:1,name:"product 1",price:34}
+const {id,name,price}=item
+console.log(name)
